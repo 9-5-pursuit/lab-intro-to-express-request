78cd3f733e18eab04fe6c522b90507412562c3ed
davep80 <davidpaq1@gmail.com>
2023-06-22 15:08:06 -0400

    11-11

--- app.js
+++ app.js
@@ -3,29 +3,36 @@ const express = require('express');
 const poke = require('./models/pokemon.json')
 const app = express();
 
-const pokemon = require("./models/pokemon.json");
-
 app.get('/', (req, res) => {
     res.send("Welcome 99 Pokemon")
 })
 app.get('/bugs', (req, res) => {
     res.send(`<h1>99 little bugs in the code</h1><a href='http://localhost:8888/bugs/101'>pull on down, patch it around</a>`)
 })
-
 app.get('/bugs/:id', (req, res) => {
 
     if (+req.params.id > 199) {
-        res.status(404).send({message: "Too many bugs!! Start over!"})
+        res.status(404).send({ message: "Too many bugs!! Start over!" })
     }
     res.status(201).send(
         `${req.params.id} little bugs in the code\n<a href='http://localhost:8888/bugs/${+req.params.id + 2}'>Pull one down, patch it around</a>`
     )
 })
-
 app.get('/pokemon', (req, res) => {
 
     res.json(
         poke
+    )
+})
+app.get('/pokemon/search', (req, res) => {
+
+    const name = req.query.name
+    let found = poke.find((item) => item.name.toLowerCase() == name.toLowerCase())
+    if (!found) {
+        res.status(404).json([])
+    }
+    res.status(201).json(
+        [found]
     )
 })
 app.get('/pokemon/:id', (req, res) => {
@@ -40,14 +47,13 @@ app.get('/pokemon/:id', (req, res) => {
         poke[+req.params.id]
     )
 })
-app.get('/pokemon/search', (req, res) => {
+app.get('/:verb/:adjective/:noun', (req, res) => {
 
-    let found = poke.find((item) => item.name.toLowerCase() == req.body.name.toLowerCase())
-    if (!found) {
-        res.status(404).json([])
-    }
-    res.status(201).json(
-        found
+    let verb = req.params.verb
+    let adjective = req.params.adjective
+    let noun = req.params.noun
+    res.status(200).send(
+        `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
     )
 })
 
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const app = require('./app')
-//const logger = require('morgan')
-//app.use(logger('dev'))
-//app.use(express.json());
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
