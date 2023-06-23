e85f5e508f6b3f238cca811540011fd37ef7217b
Michelle Harley <chell.hx@gmail.com>
2023-06-22 23:22:10 -0400

    fixed new project generator code, and created controllers for it and bugs code

--- app.js
+++ app.js
@@ -1,42 +1,17 @@
 const express = require("express");
 const app = express();
 
+// - - - CONTROLLERS - - -
+const bugController = require("./controllers/bugController");
+const nameGenController = require("./controllers/nameGenController");
+
+app.use("/", nameGenController);
+app.use("/bugs", bugController);
+
 app.get("/", (req, res) => {
   res.send("WASSAAAAAAAAAP");
 });
 
-// New Project Name Generator
-app.get("/:verb/:adjective/:noun", (req, res) => {
-  res.send(
-    `Congratulations on starting a new project called ${verb}-joyous-jellybean!`
-  );
-});
-
-// 99 Little Bugs In the Code
-app.get("/bugs", (req, res) => {
-  let title = "99 little bugs in the code";
-  let link = `<br/><a href="/bugs/101">Pull one down, Patch it around</a>`;
-  res.send(`${title} ${link}`);
-});
-
-app.get("/bugs/:numberOfBugs", (req, res) => {
-  let { numberOfBugs } = req.params;
-  const line = `${numberOfBugs} little bugs in the code`;
-  const add2 = +numberOfBugs + 2;
-  const plus2Link = `<br><a href="/bugs/${add2}">Pull one down, Patch it around</a>`;
-  const startOverLink = `<a href="/bugs">🕷️ Too many bugs! Start Over! 🕷️</a>`;
-  const message = +numberOfBugs >= 200 ? startOverLink : line + plus2Link;
-  res.send(message);
-
-  // if (+numberOfBugs >= 200) {
-  //     res.send(${startOverLink})
-  // } else {
-  //     res.send(`${line} ${plus2Link}`);
-  // }
-});
-
 // Poke-Express
-
-
 
 module.exports = app;
--- controllers/bugController.js
+++ controllers/bugController.js
@@ -2,4 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
+// 99 Little Bugs In the Code
+router.get("/", (req, res) => {
+  let title = "99 little bugs in the code";
+  let link = `<br/><a href="/bugs/101">Pull one down, Patch it around</a>`;
+  res.send(`${title} ${link}`);
+});
+
+router.get("/:numberOfBugs", (req, res) => {
+  let { numberOfBugs } = req.params;
+  const line = `${numberOfBugs} little bugs in the code`;
+  const add2 = +numberOfBugs + 2;
+  const plus2Link = `<br><a href="/bugs/${add2}">Pull one down, Patch it around</a>`;
+  const startOverLink = `<a href="/bugs">🕷️ Too many bugs! Start Over! 🕷️</a>`;
+  const message = +numberOfBugs >= 200 ? startOverLink : line + plus2Link;
+  res.send(message);
+
+  // if (+numberOfBugs >= 200) {
+  //     res.send(${startOverLink})
+  // } else {
+  //     res.send(`${line} ${plus2Link}`);
+  // }
+});
+
 module.exports = router;
