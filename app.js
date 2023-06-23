81151d7cfc49e96b14c72bd49651227fc3f796c5
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-06-22 20:02:57 -0400

    Finish 1st project started 2nd

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  );
+});
 
-app.get("/:verb/:adjective:/:noun", (req,res) =>{
-    res.send('Congratulations on starting a new project called ${}-${}-${}!')
-} )
+app.get("/bugs", (req, res) => {
+  res.send();
+});
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+  res.send();
+});
 
 module.exports = app;
