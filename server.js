ddbc9a3dd87037377459e5c10ac0d014ec839e16
Karma Ghale <karmaghale@Karmas-Air-2.cable.rcn.com>
2023-06-23 09:37:25 -0400

    Done

---
+++ server.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const app = require("./app");
+const PORT = process.env.PORT || 8888;
+
+app.listen(PORT, () => {
+  console.log("Server is running on the port " + PORT);
+});
